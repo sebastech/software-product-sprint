a70e3df3e7e754d4d0f4992536a0ff06d7e579a0
Sebastian Lopez <sebasseng@gmail.com>
2020-02-25 16:17:13 -0800

    modifications after first review

--- portfolio/src/main/webapp/game.js
+++ portfolio/src/main/webapp/game.js
@@ -1,7 +1,5 @@
 
-
-
-var myVar, canvas, ctx, x, y, dx, dy, factor, color, colorStr;
+var canvas, ctx, x, y, dx, dy, factor, color, colorStr;
 
 function init() {
     canvas = document.getElementById("myCanvas");
@@ -11,7 +9,7 @@ function init() {
     dx = 2;
     dy = -2;
     factor = 1;
-    myVar = setInterval(draw, 10);
+    setInterval(draw, 10);
     color = ["blue", "red", "green", "orange", "black", "brown", "white"];
     colorStr = color[0];
 }
@@ -26,10 +24,11 @@ function draw() {
         dy = -dy;
     }
     window.addEventListener('keydown', function (event) {
-      if(event.keyCode === 67){
-          //c pressed
+      if(event.keyCode === 67) {
+          //When users press "c" key.
           colorStr = color[Math.floor(Math.random() * 7)];
-      }else if(event.keyCode === 83){
+      } else if(event.keyCode === 83) {
+          //When users press "s" key.
            factor = Math.random();
       }
     })
@@ -41,40 +40,17 @@ function drawStar(col) {
     console.log("in draw star");
     ctx.fillStyle = col;
     ctx.beginPath();
-    //ctx.moveTo(108, 0.0);
     ctx.moveTo(x, y);
-    
-    //ctx.lineTo(141, 70);
     ctx.lineTo( x + (33 * factor), y + (70 * factor));
-    
-    //ctx.lineTo(218, 783);
     ctx.lineTo( x + (110 * factor), y + (78.3 * factor));
-    
-    //ctx.lineTo(162, 131);
     ctx.lineTo( x + (54 * factor), y + (131 * factor));
-    
-    //ctx.lineTo(175, 205);
     ctx.lineTo( x + (67 * factor), y + (205 * factor));
-    
-    
-    //ctx.lineTo(108, 170);
     ctx.lineTo(x, y + (170 * factor));
-    
-    //ctx.lineTo(41.2, 205);
     ctx.lineTo(x - (66.8 * factor), y + (205 * factor));
-    
-// ctx.lineTo(55, 131);
     ctx.lineTo(x  - (53 * factor), y + (131 * factor));
-    
-    //ctx.lineTo(1, 78);
     ctx.lineTo(x - (107 * factor), y + (78 * factor));
-    
-    //ctx.lineTo(75, 68);
     ctx.lineTo(x - (33 * factor), y + (68 * factor));
-    
-    //ctx.lineTo(108, 0);
     ctx.lineTo(x, y);
-    
     ctx.closePath();
     ctx.fill();
 }
